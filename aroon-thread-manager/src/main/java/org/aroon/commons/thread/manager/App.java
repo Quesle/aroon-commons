6609c6495bed42fe08e44b33a95b99c45030636f
Quesle <oovmanu@hotmail.com>
2014-04-09 16:51:50 +0800

    Delete the thread manager
    Delete the thread manager project

---
+++ aroon-thread-manager/src/main/java/org/aroon/commons/thread/manager/App.java
@@ -0,0 +1,13 @@
+package org.aroon.commons.thread.manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
